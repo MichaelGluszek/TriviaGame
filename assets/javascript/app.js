cb4ea0084fb1b91cd3663743743f6080e275a3f8
Mike Gluszek <mgluszek710@gmail.com>
2019-04-24 13:42:33 -0500

    html touches

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,21 @@
-$(function () {
+$("document").ready(function () {
 
-  $("startBtn").on("click", countDown.start);
+  $("#startBtn").on("click", countDown.start);
   $(".option").on("click", countDown.correctCount);
 
   $("#submitBtn").on("click", countDown.stop);
   $("#resetBtn").on("click", countDown.reset);
 
+  let period;
+  let countDown = false;
+  let corrAns = 0;
 
+    let countDown ={
+
+    time: 50,
+
+    correctCount: function() {
+// 
+    }
+  }
 });
